47dbf763707fddd4940fe23b2209acd69581c01b
ashwin2721 <136354229+ashwin2721@users.noreply.github.com>
2024-02-02 01:24:04 +0530

    netflix app.js

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import NavBar from './component/NavBar/NavBar'
+import "./App.css";
+import Banner from './component/banner/Banner';
+import Row from './component/Row/Row';
+
+function App() {
+  return (
+    <div>
+    <NavBar/>
+    <Banner/>
+    <Row/>
+    </div>
+  )
+}
+
+export default App
